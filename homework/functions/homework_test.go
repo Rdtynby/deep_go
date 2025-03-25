b71b40fad6acea2c710daa99a752009290be265d
abo <abo@mayflower.work>
2025-03-25 18:36:58 +0200

    Homework #5

--- homework/functions/homework_test.go
+++ homework/functions/homework_test.go
@@ -7,19 +7,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Map(data []int, action func(int) int) []int {
-	// need to implement
-	return nil
+type Number interface {
+	int | int8 | int16 | int32 | int64
 }
 
-func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+func Map[T Number](data []T, action func(T) T) []T {
+	if data == nil {
+		return nil
+	}
+
+	result := make([]T, len(data))
+
+	for k, v := range data {
+		result[k] = action(v)
+	}
+
+	return result
 }
 
-func Reduce(data []int, initial int, action func(int, int) int) int {
-	// need to implement
-	return 0
+func Filter[T Number](data []T, action func(T) bool) []T {
+	if data == nil {
+		return nil
+	}
+
+	result := make([]T, 0)
+
+	for _, v := range data {
+		if action(v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func Reduce[T Number](data []T, initial T, action func(T, T) T) T {
+	result := action(0, initial)
+
+	for _, v := range data {
+		result = action(v, result)
+	}
+
+	return result
 }
 
 func TestMap(t *testing.T) {
