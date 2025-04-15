31e54150d26312fc6faef58c184f78525d24752b
abo <abo@mayflower.work>
2025-04-15 22:10:21 +0300

    Homework #8

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +12,38 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errors []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	result := strconv.Itoa(len(e.errors)) + " errors occured:\n"
+
+	for _, err := range e.errors {
+		result += "\t* " + err.Error()
+	}
+
+	result += "\n"
+
+	return result
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	switch e := any(err).(type) {
+	case *MultiError:
+		e.errors = append(e.errors, errs...)
+
+		return e
+	default:
+		return &MultiError{errs}
+	}
+}
+
+func (e *MultiError) Unwrap() error {
+	if len(e.errors) == 1 {
+		return nil
+	}
+
+	return &MultiError{e.errors[0 : len(e.errors)-1]}
 }
 
 func TestMultiError(t *testing.T) {
@@ -30,4 +53,14 @@ func TestMultiError(t *testing.T) {
 
 	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
 	assert.EqualError(t, err, expectedMessage)
+
+	err2 := errors.Unwrap(err)
+	expectedMessage2 := "1 errors occured:\n\t* error 1\n"
+	assert.EqualError(t, err2, expectedMessage2)
+	err3 := errors.Unwrap(err2)
+	assert.Equal(t, err3, nil)
+
+	err4 := Append(nil, &os.SyscallError{"error", errors.New("error 4")})
+	expectedMessage4 := "1 errors occured:\n\t* error: error 4\n"
+	assert.EqualError(t, err4, expectedMessage4)
 }
