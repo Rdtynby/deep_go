48a4e5f8d6a7bb2cf43d73f09ed23bb1407377b0
abo <abo@mayflower.work>
2025-04-24 22:05:52 +0300

    Homework #10

--- homework/allocator/homework_test.go
+++ homework/allocator/homework_test.go
@@ -10,16 +10,35 @@ import (
 
 // go test -v homework_test.go
 
-func Defragment(memory []byte, pointers []unsafe.Pointer) {
-	// need to implement
+type Number interface {
+	byte | int | int8 | int16 | int32 | int64
+}
+
+func Defragment[T Number](memory []T, pointers []unsafe.Pointer) {
+	currentPointer := 0
+	changes := make(map[unsafe.Pointer]unsafe.Pointer)
+
+	for index, pointer := range pointers {
+		if changes[pointer] == nil {
+			changes[pointer] = unsafe.Pointer(&memory[currentPointer])
+
+			tmp := memory[currentPointer]
+			memory[currentPointer] = *(*T)(pointers[index])
+			*(*T)(pointers[index]) = tmp
+
+			currentPointer++
+		}
+
+		pointers[index] = changes[pointer]
+	}
 }
 
 func TestDefragmentation(t *testing.T) {
 	var fragmentedMemory = []byte{
-		0xFF, 0x00, 0x00, 0x00,
-		0x00, 0xFF, 0x00, 0x00,
-		0x00, 0x00, 0xFF, 0x00,
-		0x00, 0x00, 0x00, 0xFF,
+		0xF0, 0x00, 0x00, 0x00,
+		0x00, 0xF1, 0x00, 0x00,
+		0x00, 0x00, 0xF2, 0x00,
+		0x00, 0x00, 0xF4, 0xF3,
 	}
 
 	var fragmentedPointers = []unsafe.Pointer{
@@ -27,6 +46,8 @@ func TestDefragmentation(t *testing.T) {
 		unsafe.Pointer(&fragmentedMemory[5]),
 		unsafe.Pointer(&fragmentedMemory[10]),
 		unsafe.Pointer(&fragmentedMemory[15]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+		unsafe.Pointer(&fragmentedMemory[14]),
 	}
 
 	var defragmentedPointers = []unsafe.Pointer{
@@ -34,11 +55,51 @@ func TestDefragmentation(t *testing.T) {
 		unsafe.Pointer(&fragmentedMemory[1]),
 		unsafe.Pointer(&fragmentedMemory[2]),
 		unsafe.Pointer(&fragmentedMemory[3]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+		unsafe.Pointer(&fragmentedMemory[4]),
 	}
 
 	var defragmentedMemory = []byte{
-		0xFF, 0xFF, 0xFF, 0xFF,
+		0xF0, 0xF1, 0xF2, 0xF3,
+		0xF4, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	Defragment(fragmentedMemory, fragmentedPointers)
+	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
+	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
+}
+
+func TestDefragmentationInt64(t *testing.T) {
+	var fragmentedMemory = []int64{
+		0xF0, 0x00, 0x00, 0x00,
+		0x00, 0xF1, 0x00, 0x00,
+		0x00, 0x00, 0xF2, 0x00,
+		0x00, 0x00, 0xF4, 0xF3,
+	}
+
+	var fragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[5]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+		unsafe.Pointer(&fragmentedMemory[15]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+		unsafe.Pointer(&fragmentedMemory[14]),
+	}
+
+	var defragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[1]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+		unsafe.Pointer(&fragmentedMemory[3]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+		unsafe.Pointer(&fragmentedMemory[4]),
+	}
+
+	var defragmentedMemory = []int64{
+		0xF0, 0xF1, 0xF2, 0xF3,
+		0xF4, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00,
 	}
