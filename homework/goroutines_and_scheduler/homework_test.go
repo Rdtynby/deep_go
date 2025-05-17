f5edafc262b5de8fe821d8d6c2da42f177520383
abo <abo@mayflower.work>
2025-05-17 22:29:14 +0300

    Homework #12

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -19,42 +19,12 @@ type SchedulerWithSharing struct {
 	schedulers []Scheduler
 }
 
-func heapifyStep(arr []Task, n, i int) {
-	for {
-		left := 2*i + 1
-		right := 2*i + 2
-		largest := i
-
-		if left < n && arr[left].Priority > arr[largest].Priority {
-			largest = left
-		}
-
-		if right < n && arr[right].Priority > arr[largest].Priority {
-			largest = right
-		}
-
-		if largest == i {
-			break
-		}
-
-		arr[i], arr[largest] = arr[largest], arr[i]
-		i = largest
-	}
-}
-
-func heapify(arr []Task) {
-	n := len(arr)
-
-	for i := n/2 - 1; i >= 0; i-- {
-		heapifyStep(arr, n, i)
-	}
-}
-
 func NewSchedulerWithSharing() SchedulerWithSharing {
-	schedulers := make([]Scheduler, 0)
-	schedulers = append(schedulers, NewScheduler())
-	schedulers = append(schedulers, NewScheduler())
-	schedulers = append(schedulers, NewScheduler())
+	schedulers := []Scheduler{
+		NewScheduler(),
+		NewScheduler(),
+		NewScheduler(),
+	}
 
 	return SchedulerWithSharing{
 		schedulers: schedulers,
